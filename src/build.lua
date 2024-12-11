fe3da3570fd69db7938b6718ff71b0b69968c133
Gustavo <146868632+GustavoFurtad2@users.noreply.github.com>
2024-12-10 23:08:11 -0300

    initial commit

---
+++ src/build.lua
@@ -0,0 +1,21 @@
+function build(files)
+
+    local content = ""
+    local build = io.open("builds/build.lua", "w")
+
+    for k, v in next, files do
+
+        local file = io.open(v .. ".lua")
+        content = content .. file:read("*a") .. "\n\n"
+        file:close()
+
+    end
+
+    build:write(content)
+end
+
+local files = {}
+
+build(files)
+
+return
